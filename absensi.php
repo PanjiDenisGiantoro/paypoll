706d1fa8e035c682d2ddd83d1aac964a5dc465ad
PanjiDenisGiantoro <32445688+PanjiDenisGiantoro@users.noreply.github.com>
2019-11-25 11:12:29 +0700

    absensi done

--- absensi.php
+++ absensi.php
@@ -48,14 +48,13 @@ Error <strong>Proses Gagal</strong>
 <?php
 }
 ?>
-<a href="absensi.php?view=tambah" class="btn btn-primary" style="float: right; margin-right: 30px ; margin-top: 40px" >Tambah Data Karyawan</a>
+<a href="absensi.php?view=tambah" class="btn btn-primary" style="float: right; margin-right: 30px ; margin-top: 40px" >Tambah Data Absensi Karyawan</a>
 <table  class="table " >
 </div>
 <thead>
 <tr>
 
 <th align='center'>No</th>
-<th align='center'>Id</th>
 <th align='center'>Id Karyawan</th>
 <th align='center'>Nama Karyawan</th>
 <th align='center'>Jabatan</th>
@@ -66,18 +65,17 @@ Error <strong>Proses Gagal</strong>
 </thead>
 <?php
 
-$sql = mysqli_query($konek,"SELECT a.nama as nama ,a.jabatan as jabatan ,b.id as id,b.tglAbsen as tglAbsen,b.keterangan as keterangan from karyawans a join absens b on a.id = b.idKaryawan  ");
+$sql = mysqli_query($konek,"SELECT a.nama as nama ,a.jabatan as jabatan ,b.idKaryawan as idKaryawan,b.tglAbsen as tglAbsen,b.keterangan as keterangan,a.id as id from karyawans a join absens b on a.id = b.idKaryawan  ");
 $no=1;
 while($d= mysqli_fetch_array($sql)){
 echo 
 "<tr>
 <td width='40px' align='center'>$no</td>
-<td align='center'>$d[id]</td>
+<td align='center'>$d[idKaryawan]</td>
 <td align='center'>$d[nama]</td>
 <td align='center'>$d[jabatan]</td>
 <td align='center'>$d[tglAbsen]</td>
 <td align='center'>$d[keterangan]</td>
-
 <td width='40px' align='center'>
 <a class='btn btn-warning btn-sm fa fa-edit' href='absensi.php?view=edit&id=$d[id]' ></a>
 <a class='btn btn-danger btn-sm fa fa fa-eraser' href='aksi_absensi.php?act=del&id=$d[id]'></a>
@@ -93,12 +91,9 @@ $no++;
 </div>
 </div>
 </div>
-
-
 <?php
 break;
 case "tambah";  
-
 $query = "SELECT max(id) as id FROM absens";
 $hasil = mysqli_query($konek,$query);
 $data = mysqli_fetch_array($hasil);
@@ -113,8 +108,6 @@ if(isset($_GET['e']) && $_GET['e']=='bl'){
 <div class="alert alert-danger" role="alert">
 Peringatan<strong>Form Belum Lengkap</strong>
 </div>
-
-
  <?php
 }
 ?>
@@ -149,93 +142,79 @@ Peringatan<strong>Form Belum Lengkap</strong>
 <div class="card-header">
 <strong>Absensi</strong>
 <small> Tambah</small>
-
 </div>
 <div style='margin-left:30px;'>
 <div class="card-body card-block row">
 <form method="post" action="aksi_absensi.php?act=insert" class="form-horizontal">
 </div>
-<table  width="900px">
-
-<tr>
-<td>
-<div class="form-group">
-<label for="id" class=" form-control-label">Id Absensi</label>
-<input type="text" value="<?php echo $kodeBarang; ?>" name="id" placeholder="Masukkan ID Absensi" class="form-control"style="width: 400px" readonly>
-</div>
-</td>
-<td> 
-<div class="form-group">
-<label for="nama" class=" form-control-label">Nama Karyawan</label>
-<input type="text" name="nama" placeholder="Masukkan Nama Karyawan" class="form-control"style="width: 400px" >
-</div>
-</td>
-</tr>
-<tr>
-
-<td> 
-<div class="form-group">
-<label for="id" class=" form-control-label">Id Karyawan</label>
-<select name="id" id="id"  class="form-control-sm form-control" style="width: 400px"  onchange="changeValue(this.value)">
-     <option value="0">Please select</option>
-    <?php
-    $result = mysql_query("SELECT b.id as id from karyawans a join absens b on a.id = b.idKaryawan");   
+    <table  width="900px">
+      <tr>
+        <td>
+            <div class="form-group">
+                <label for="id" class=" form-control-label">Id Karyawan</label>
+            <select name="idKaryawan" id="idKaryawan" class=" form-control" onchange="changeValue(this.value)" style="width: 400px" >
+        <option value=0>-Pilih-</option>
+         <?php
+         $konek = mysqli_connect("localhost","root","","payrol");
+    $result = mysqli_query($konek,"SELECT * from karyawans");   
     $jsArray = "var dtMhs = new Array();\n";       
-    while ($row = mysql_fetch_array($result)) {   
-        echo '<option value="' 
-        . $row['id'] . '">' . $row['id'] . '</option>';   
-        $jsArray 
-        .= "dtMhs['" . $row['id'] . "'] = {nama:'" . addslashes($row['nama']) 
-        . "',jabatan:'".addslashes($row['jabatan'])."'};\n";   
+    while ($row = mysqli_fetch_array($result)) {   
+        echo '<option value="'.$row['id'].'">'.$row['id'].'</option>';   
+        $jsArray .= "dtMhs['".$row['id']."'] = {nama:'".addslashes($row['nama'])."',jabatan:'".addslashes($row['jabatan'])."'};\n";   
     }     
     ?>   
- </select>
-
-</div>
-</td>
-</tr>
-<tr>
-<td>
+        </select>
+    </td>
+    <td>
 <div class="form-group">
-<label for="status" class=" form-control-label">Status</label>
-<select name="status"  class="form-control-sm form-control" style="width: 400px">
-     <option value="0">Please select</option>
-     <option value="TETAP">TETAP</option>
-     <option value="KONTRAK">KONTRAK</option>
- </select>
-</div>
+<label for="tglAbsen" class=" form-control-label">Tanggal Absen</label>
+<input type="date" name="tglAbsen"  placeholder="Masukkan Tanggal Absen Anda" class="form-control"style="width: 400px" >
 </div>
 </td>
-<td>
-<div class="form-group">
-<label for="tempatJabatan" class=" form-control-label">Tempat Jabatan</label>
-<select name="tempatJabatan"  class="form-control-sm form-control" style="width: 400px">
-     <option value="0">Please select</option>
-     <option value="SD">SD</option>
-     <option value="SMP">SMP</option>
-      <option value="SMA">SMA</option>
- </select>
-</div>
-</td>
-</tr>
-<tr>
-</tr>
-</table>
+   
+      </tr>
+      <tr>
+        <td>
+        <div class="form-group">
+  <label for="id" class=" form-control-label">Nama Karyawan</label>
+      <input type="text" name="nama" id="nama" class="form-control"style="width: 400px"/></td>
+       <td>
+        <div class="form-group">
+  <label for="id" class=" form-control-label">jabatan Karyawan</label>
+      <input type="text" name="jabatan" id="jabatan" class="form-control"style="width: 400px"/></td>
+    </div>
+      </tr>
+      <tr>
+        <td>
+        <div class="form-group">
+        <label for="keterangan" class=" form-control-label">keterangan absen</label>
+        <textarea name="keterangan" id="keterangan" rows="9" placeholder="Content..." class="form-control" style="width: 400px"></textarea>
+        </td>
+      </tr>
+    </table>
+      <script type="text/javascript">   
+    <?php echo $jsArray; ?> 
+    function changeValue(id){ 
+    document.getElementById('nama').value = dtMhs[id].nama; 
+    document.getElementById('jabatan').value = dtMhs[id].jabatan; 
+    }; 
+    </script>
 <div class="card-body">
 <input type="submit" class="btn btn-primary" value="simpan">
 
 
-<a class="btn btn-danger" href="karyawan.php">kembali</a>
+<a class="btn btn-danger" href="absensi.php">kembali</a>
 </div>
 </div>
 </div>
+
 </form>
+
 </div>
 <?php
 break;
 case "edit";
-
-$sqlEdit = mysqli_query($konek,"select * from karyawans where id='$_GET[id]'");
+$sqlEdit = mysqli_query($konek,"select * from absens a join karyawans b on a.idKaryawan = b.id where a.idKaryawan='$_GET[id]'");
 $e = mysqli_fetch_array($sqlEdit);
 $query = "SELECT max(id) as id FROM karyawans";
 $hasil = mysqli_query($konek,$query);
@@ -261,7 +240,7 @@ $kodeBarang = $char . sprintf("%03s", $noUrut);
 <li class="list-inline-item seprate">
 <span>/</span>
 </li>
-<li class="list-inline-item">Edit Karyawan</li>
+<li class="list-inline-item">Edit Absensi Karyawan</li>
 </ul>
 </div>
 </div>
@@ -280,101 +259,48 @@ $kodeBarang = $char . sprintf("%03s", $noUrut);
 </div>
 <div style='margin-left:30px;'>
 <div class="card-body card-block row">
-<form action="aksi_karyawan.php?act=update" method="post" class="form-horizontal">
+<form action="aksi_absensi.php?act=update" method="post" class="form-horizontal">
 </div>
-<table  width="900px">
-
-<tr>
-<td>
+  <table  width="900px">
+      <tr>
+        <td>
+            <div class="form-group">
+                <label for="idKaryawan" class=" form-control-label">Id Karyawan</label>
+            <input type="text" name="idKaryawan" value="<?php echo $e['idKaryawan']?>"readonly  placeholder="Masukkan Nama Karyawan" class="form-control"style="width: 400px">
+    </td>
+    <td>
 <div class="form-group">
-<label for="id" class=" form-control-label">Id Karyawan</label>
-<input type="text" value="<?php echo $e['id']; ?>" name="id" placeholder="Masukkan ID Karyawan" class="form-control"style="width: 400px" readonly>
+<label for="tglAbsen" class=" form-control-label">Tanggal Absen</label>
+<input type="date" name="tglAbsen"  value="<?php  echo $e['tglAbsen']?>" placeholder="Masukkan Tanggal Absen Anda" class="form-control"style="width: 400px" >
 </div>
 </td>
-<td> 
-<div class="form-group">
-<label for="nama" class=" form-control-label">Nama Karyawan</label>
-<input type="text" name="nama" value="<?php echo $e['nama']?>" placeholder="Masukkan Nama Karyawan" class="form-control"style="width: 400px">
-</div>
-</td>
-</tr>
-<tr>
-<td>
-<div class="form-group">
-<label for="CardNumber" class=" form-control-label">Card Id</label>
-<input type="text" name="CardNumber" value="<?php  echo $e['CardNumber']?>" placeholder="Masukkan Card Id Anda" class="form-control"style="width: 400px" >
-</div>
-</td>
-<td> 
-
-<div class="form-group">
-<label for="jabatan" class=" form-control-label">Jabatan</label>
-<select name="jabatan" class="form-control-sm form-control" style="width: 400px">
-     <?php $jabatann = $e['jabatan']; ?>
-     <option <?php echo ($jabatann == 'OB') ? "selected": "" ?>>OB</option>
-     <option <?php echo ($jabatann == 'GURU') ? "selected": "" ?>>GURU</option>
- </select>
-</div>
-</td>
-</tr>
-<tr>
-<td>
-
-<div class="form-group">
-<label for="status" class=" form-control-label">Status</label>
-<select name="status"  class="form-control-sm form-control" style="width: 400px">
-   <?php $statuss = $e['status']; ?>
-     <option <?php echo ($statuss == 'TETAP') ? "selected": "" ?>>TETAP</option>
-     <option <?php echo ($statuss == 'KONTRAK') ? "selected": "" ?>>KONRAK</option>
-
- </select>
-</div>
-</div>
-</td>
-<td>
-<div class="form-group">
-<label for="tempatJabatan" class=" form-control-label">Tempat Jabatan</label>
-<select name="tempatJabatan"  class="form-control-sm form-control" style="width: 400px">
-     <?php $jab = $e['tempatJabatan']; ?>
-     <option <?php echo ($jab == 'SD') ? "selected": "" ?>>SD</option>
-     <option <?php echo ($jab == 'SMP') ? "selected": "" ?>>SMP</option>
-     <option <?php echo ($jab == 'SMA') ? "selected": "" ?>>SMA</option> 
- </select>
-</div>
-</td>
-</tr>
-<tr>
-<td>
-<div class="form-group">
-<label for="tglMasuk" class=" form-control-label">Tanggal Masuk</label>
-<input type="date" name="tglMasuk" value="<?php  echo $e['tglMasuk']?>"  placeholder="Masukkan Tanggal Masuk Anda" class="form-control"style="width: 400px" >
-</div>
-</td>
-<td>
-<div class="form-group">
-<label for="tglKeluar" class=" form-control-label">Tanggal Keluar</label>
-<input type="date" name="tglKeluar" value="<?php  echo $e['tglKeluar']?>" placeholder="Masukkan Tanggal Keluar Anda" class="form-control"style="width: 400px" >
-</div>
-</td>
-</tr>
-<tr>
-<td>
-<div class="form-group">
-<label for="bpjsKes" class=" form-control-label">BPJS Kesehatan</label>
-<input type="number" name="bpjsKes" value="<?php echo $e['bpjsKes']; ?>" placeholder="Masukkan bpjsKesesehatan Anda" class="form-control"style="width: 400px" >
-</div>
-</td>
-<td>
-<div class="form-group">
-<label for="bpjsKet" class=" form-control-label">BPJS Ketenagakerjaan</label>
-<input type="number" name="bpjsKet" value="<?php echo $e['bpjsKet']; ?>" placeholder="Masukkan bpjs Ketenagakerjaan Anda" class="form-control"style="width: 400px" >
-</div>
-</td>
-</tr>
-</table>
+   
+      </tr>
+      <tr>
+        <td>
+        <div class="form-group">
+  <label for="id" class=" form-control-label">Nama Karyawan</label>
+      <input type="text" name="nama" id="nama" value="<?php echo $e['nama']?>" readonly class="form-control"style="width: 400px"/>
+        </td>
+       <td>
+        <div class="form-group">
+  <label for="id" class=" form-control-label">jabatan Karyawan</label>
+      <input type="text" name="jabatan" id="jabatan" value="<?php echo $e['jabatan']?>"readonly class="form-control"style="width: 400px"/>
+  </td>
+    </div>
+      </tr>
+      <tr>
+        <td>
+        <div class="form-group">
+        <label for="keterangan" class=" form-control-label">keterangan absen</label>
+        <textarea name="keterangan" id="keterangan"rows="9" placeholder="Content..." class="form-control" style="width: 400px"><?php echo $e['keterangan']?></textarea>
+        </td>
+      </tr>
+    </table>
+    
 <div class="card-body">
-<input type="submit" class="btn btn-primary " value="Simpan">
-<a class="btn btn-danger" href="karyawan.php">kembali</a>
+<input type="submit" class="btn btn-primary" value="simpan">
+<a class="btn btn-danger" href="absensi.php">kembali</a>
 </div>
 </div>
 </div>
---
+++ aksi_absensi.php
@@ -0,0 +1,60 @@
+<?php
+
+session_start();
+include "koneksi/koneksi.php";
+
+if(!isset($_SESSION['login'])){
+	header('location:login.php');
+}
+if(isset($_GET['act'])){
+if($_GET['act'] =='insert'){
+	$id = $_POST['id'];
+	$idKaryawan = $_POST['idKaryawan'];
+	$tglAbsen = $_POST['tglAbsen'];
+	$keterangan = $_POST['keterangan'];
+	if($id==''|| $idKaryawan==''||$tglAbsen==''||$keterangan==''){
+		header('location:absensi.php?view=tambah&e=bl');
+	}else{
+		$simpan = mysqli_query($konek,"insert into absens(id,idKaryawan,tglAbsen,keterangan) values ('$id','$idKaryawan','$tglAbsen','$keterangan')");
+
+		if($simpan){
+			header('location:absensi.php?e=sukses');
+		}else{
+
+			header('location:absensi.php?e=gagal');
+		}
+	}
+
+}
+else if ($_GET['act']=='update'){
+	$idKaryawan = $_POST['idKaryawan'];
+	$tglAbsen = $_POST['tglAbsen'];
+	$keterangan = $_POST['keterangan'];
+	if($idKaryawan==''||$tglAbsen==''||$keterangan==''){
+		header('location:absensi.php?view=tambah&e=bl');
+
+	}else{
+		$update = mysqli_query($konek,"update absens set tglAbsen='$tglAbsen',keterangan='$keterangan' where idKaryawan='$idKaryawan'");
+
+		if($update){
+			header('location:absensi.php?e=sukses');
+		}else{
+
+			header('location:absensi.php?e=gagal');
+		}
+	}
+}
+else if ($_GET['act'] == 'del'){
+	$hapus = mysqli_query($konek,"delete from absens where idKaryawan = '$_GET[id]'");
+
+	if($hapus){
+		// echo var_dump("delete from absens where idKaryawan = '$_GET[id]'");die();
+		header('location:absensi.php?e=sukses');
+	}else{
+		header('location:absensi.php?e=gagal');
+	}
+}
+}
+
+
+?>
